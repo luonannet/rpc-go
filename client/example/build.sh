080efe9989eaebfd869a699b5e3fa1b17fe11b78
Jun Luo <junl@ubuntu.none>
2016-12-09 11:57:28 +0800

    fixed buildTime

--- client/example/build.sh
+++ client/example/build.sh
@@ -4,4 +4,4 @@ BuildTime=`date -u '+%Y-%m-%d_%I:%M:%S%p'`
 GitHash=`git rev-parse --short HEAD`
 Version="0.1.0-dev"
 
-go build -v -ldflags="-X main.GitHash=${GitHash} -X main.buildTime=${BuildTime} -X main.Version=${dev}" -o client main.go
+go build -v -ldflags="-X main.GitHash=${GitHash} -X main.BuildTime=${BuildTime} -X main.Version=${dev}" -o client main.go
